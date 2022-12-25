354306d5174a5f5f991e2fa30efe1487eabccee9
eteRnaL198 <ikuya313198@gmail.com>
2022-12-25 19:07:54 +0900

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { CafeteriaList } from "containers/Cafeteria";
 import { Header } from "containers/Header";
+import { Login } from "containers/Login";
 import Div100vh from "react-div-100vh";
+import { User } from "common/type";
 
 function App() {
+  const [user, setUser] = useState<User>();
+
   return (
     <Div100vh className="bg-orange-100 flex flex-col overflow-y-auto">
       <Header />
-      <CafeteriaList />
+      {user ? (
+        <>
+          <CafeteriaList />
+        </>
+      ) : (
+        <Login setUser={setUser} />
+      )}
     </Div100vh>
   );
 }
